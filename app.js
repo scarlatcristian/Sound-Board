8308d1105dfb5ef0b8c96a7037157877a3bd438d
cristi <scarlat.cristian.gabriel@gmail.com>
2023-03-01 16:44:09 +0800

    added CSS and JS

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 "use strict";
+
+const notes = ["do", "re", "mi", "fa", "sol", "la", "si"];
+
+const stopNotes = () => {
+  notes.forEach((note) => {
+    const sound = document.getElementById(note);
+
+    sound.pause();
+    sound.currentTime = 0;
+  });
+};
+
+notes.forEach((note) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+
+  btn.innerText = note;
+
+  btn.addEventListener("click", () => {
+    stopNotes();
+
+    document.getElementById(note).play();
+  });
+
+  document.getElementById("buttons").appendChild(btn);
+});
